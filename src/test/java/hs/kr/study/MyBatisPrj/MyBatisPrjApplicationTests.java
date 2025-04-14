ad65a4a4f9418f97e7fcf9e52790b72b35d4d26d
Najisu <jisusunny0225@gmail.com>
2025-04-14 10:46:14 +0900

    first commit

---
+++ src/test/java/hs/kr/study/MyBatisPrj/MyBatisPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.kr.study.MyBatisPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
