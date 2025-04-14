909d07785761183bf8c9aeff25ebcf3f66a1ffd4
Najisu <jisusunny0225@gmail.com>
2025-04-14 11:03:09 +0900

    로그인 기능 추가

---
+++ src/main/java/hs/kr/study/MyBatisPrj/DTO/LoginDTO.java
@@ -0,0 +1,11 @@
+package hs.kr.study.MyBatisPrj.DTO;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+
+    private String user_id;
+    private String user_pw;
+
+}
---
+++ src/main/java/hs/kr/study/MyBatisPrj/LoginController.java
@@ -0,0 +1,35 @@
+package hs.kr.study.MyBatisPrj;
+
+import hs.kr.study.MyBatisPrj.DTO.LoginDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import static java.lang.Integer.parseInt;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/Login")
+    public String login(){
+        return "Login_input";
+    }
+
+    @PostMapping("/Login_input")
+    public String login_input(@ModelAttribute LoginDTO dto, Model model){
+        String result = "wrong!";
+
+        if(dto.getUser_id().equals("kim") && parseInt(dto.getUser_pw()) == 1234){
+            result = "Welcome!";
+        }
+
+        System.out.println("아이디 : " + dto.getUser_id());
+        System.out.println("비밀번호 : " + dto.getUser_pw());
+
+        model.addAttribute("result", result);
+        return "Login_result";
+    }
+
+}
